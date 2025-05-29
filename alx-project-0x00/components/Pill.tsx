865953643c8c6cecd941ed4a42785f24976b4a97
Besomi22 <sbelllali000@gmail.com>
2025-05-29 11:40:23 +0100

    Updated the Pill file

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,25 +1,12 @@
-// const Pill: React.FC = () => {
-//   return (
-//     <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
-//       <p className=" text-sm ">Title</p>
-//     </div>
-//   )
-// }
+import { PillProps } from "@/interfaces";
 
-// export default Pill;
-
-import React from "react";
-
-interface PillProps {
-  title: string;
+const Pill: React.FC<PillProps> = ({title}) => {
+  return (
+    <div className=" flex justify-center  items-center bg-gray-700 px-3 w-auto h-[27px] rounded-full">
+      <p className=" text-sm ">{title}</p>
+    </div>
+  )
 }
 
-const Pill: React.FC<PillProps> = ({ title }) => {
-  return (
-    <span className="px-3 py-1 bg-gray-700 text-sm rounded-full font-medium">
-      {title}
-    </span>
-  );
-};
-
 export default Pill;
+
